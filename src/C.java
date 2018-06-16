ee78020133de544f985b55459a95ea0fccf484ce
min <britz704@naver.com>
2018-06-16 19:46:12 +0900

    devC version

--- src/C.java
+++ src/C.java
@@ -1,10 +1,22 @@
-#include<stdio.h>
+/*#include<stdio.h>
 int main()
 {
 printf("숫자 입력:\n");
 scanf("%d",&a);
 
 printf("첫번째: %d",a);
+getchar();
+getchar();
+return 0;
+} 수정전*/
+#include<stdio.h>
+int main()
+{
+printf("숫자 입력:\n");
+scanf("%d",&a);
+printf("숫자 입력:\n");
+scanf("%d",&b);
+printf("첫번째: %d","두번째 :%d",a,b);
 getchar();
 getchar();
 return 0;
