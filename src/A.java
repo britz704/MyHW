2386ef6b3104b42565671b6ec73dc3e5842852de
min <britz704@naver.com>
2018-06-16 19:37:12 +0900

    first version

---
+++ src/A.java
@@ -0,0 +1,50 @@
+/*#include "stdafx.h"
+#include "stdio.h"
+#include "stdlib.h"
+#include "math.h"
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	int a,b,c;
+	printf("첫번째 수를 입력하세요:\n");
+	scanf("%d",&a);
+	printf("두번째 수를 입력하세요:\n");
+	scanf("%d",&b);
+	c=a+b;
+	printf("%d+%d=%d",c);
+	c=a-b;
+	printf("%d",c);
+	c=a*b;
+	printf("%d",c);
+	
+	getchar();
+	getchar();
+	
+	return 0;
+}*/
+#include "stdafx.h"
+#include "stdio.h"
+#include "stdlib.h"
+#include "math.h"
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	int a,b,c;
+	printf("첫번째 수를 입력하세요:\n");
+	scanf("%d",&a);
+	printf("두번째 수를 입력하세요:\n");
+	scanf("%d",&b);
+	c=a+b;
+	printf("%d+%d=%d",c);
+	c=a-b;
+	printf("%d",c);
+	c=a*b;
+	printf("%d",c);
+	c=a/b;
+	printf("%d",c);
+	getchar();
+	getchar();
+	
+	return 0;
+}
+
---
+++ src/C.java
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+printf("숫자 입력:\n");
+scanf("%d",&a);
+
+printf("첫번째: %d",a);
+getchar();
+getchar();
+return 0;
+}
